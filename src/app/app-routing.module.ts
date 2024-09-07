ac7da0b9556ffbf83ace2143277c6767cd9ccdc6
Biswajeet <biswajeet163@gmail.com>
2024-09-07 20:59:49 +0530

    absdddddddddddddddddd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AComponent }, // Default route
+  { path: 'b', component: BComponent },
+  { path: 'c', component: CComponent },
+  { path: 'd', component: AComponent } // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponent,
+    BComponent,
+    CComponent
   ],
   imports: [
     BrowserModule,
